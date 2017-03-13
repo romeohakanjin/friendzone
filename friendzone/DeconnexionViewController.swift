a8d53d2ea2e6f9d7e79cfcfce07dadf94aa2e2f9
bioski1 <bioski1@gmail.com>
2017-03-13 12:07:08 +0100

    design

---
+++ friendzone/DeconnexionViewController.swift
@@ -0,0 +1,49 @@
+//
+//  DeconnexionViewController.swift
+//  friendzone
+//
+//  Created by Julien Xu on 13/03/2017.
+//  Copyright © 2017 julien. All rights reserved.
+//
+
+import UIKit
+
+class DeconnexionViewController: UINavigationController {
+
+    var config = Config()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Enleve une clé du dico
+        config.defaults.removeObject(forKey: "name")
+        print(config.defaults.dictionaryRepresentation().keys.count)
+
+        //Redirection
+        DispatchQueue.main.async {
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            
+            let nextViewController = storyBoard.instantiateViewController(withIdentifier: "Connexion_ID") as UIViewController
+            self.present(nextViewController, animated:true, completion:nil)
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
